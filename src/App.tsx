4d7cd334d46c5cc269132a8b74ae5c7198fc45ea
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-07-19 14:57:33 +0900

    background color update

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import './App.css'
-import Routes from './Routes';
-import { AlertMessageProvider } from './contexts/AlertMessageContext';
-import Loading from './components/Loading';
-import AlertMessage from './components/AlertMessage';
-import { COLOR_BACKGROUND, COLOR_BLACK, COLOR_PRIMARY } from './utils/constants';
+import Routes from './Routes'
+import { AlertMessageProvider } from './contexts/AlertMessageContext'
+import Loading from './components/Loading'
+import AlertMessage from './components/AlertMessage'
+import { COLOR_BACKGROUND, COLOR_BLACK, COLOR_PRIMARY } from './utils/constants'
 
 let theme = createTheme({
   palette: {
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,12 +2,13 @@ import { Box, Stack } from "@mui/material";
 import { Outlet } from "react-router";
 // import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import ScrollFab from "../components/ScrollFab";
+import { COLOR_BACKGROUND } from "../utils/constants";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function MainLayout() {
   return (
-    <Box sx={{ minHeight: '100vh' }} position="relative">
+    <Box sx={{ minHeight: '100vh' }} position="relative" bgcolor={COLOR_BACKGROUND}>
       <Stack sx={{ minHeight: 'inherit' }} position="relative" zIndex={20}>
         {/* Header */}
         <Navbar />
